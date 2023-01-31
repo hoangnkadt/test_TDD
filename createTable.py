42c57e7902de05e25d28d9725b266174155f9304
hoangnkadt <20034079@sv.ussh.edu.vn>
2023-01-31 10:56:20 +0700

    delete main and create creataTable

--- createTable.py
+++ createTable.py
@@ -1,1 +1,12 @@
-from input.permutatedStatus.main import *
+from src.importDataPostgres import importData
+from input.permutatedStatus.main import get_query
+from src.exportData import exportData
+from configs.config import get_config_export,get_config_import
+def run():
+  config_export = get_config_export()
+  config_import = get_config_import()
+  query, nameLink, nameTable, nameTableDatabase = get_query()
+  exportData(config_export,query,nameTable,nameLink)
+  importData(config_import,nameLink,nameTable,nameTableDatabase)
+if __name__ == '__main__':
+  run()
